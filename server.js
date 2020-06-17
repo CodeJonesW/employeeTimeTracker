1db83dc53b414f5861ad9f7a269440fbece50fd2
Will Jones <willjones2010@gmail.com>
2020-06-17 12:44:35 -0400

    reworking with tutorial

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-require("./routes/apiRoutes")(app);
+const app = express();
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
