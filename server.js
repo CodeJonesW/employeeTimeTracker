5813a505e346e666ebf29a9e57f81e65ab5f78e8
Will Jones <willjones2010@gmail.com>
2020-06-18 01:41:47 -0400

    file name update

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 });
 
 //Require Routes
-require("./routes/user.routes")(app);
+require("./routes/employee.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
